c2a75075c92cc0d0ced5e4c54ee11fede6e812d6
nituudivani <nitindivani2711@gmail.com>
2024-10-11 12:50:43 +0530

    commit on 11/10/2024

---
+++ Fontend/src/home/left/Users.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import User from './User'
+
+function Users() {
+  return (
+    <div className=' my-2 flex-nitin overflow-y-auto' style={{maxHeight:'calc(84vh - 1vh)'}}>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+     
+    </div>
+  )
+}
+
+export default Users
